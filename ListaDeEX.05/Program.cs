9eed56e3b43570090bcc2d97896d901151be650b
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-12 15:41:20 -0300

    ADD ex 05

---
+++ ListaDeEX.05/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.Write("Digite o salario base do colaborador: ");
+        double salarioBase = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.Write("Digite o percentual sobre o total de vendas: ");
+        double percentual = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.Write("Digite o total de vendas do colaborador: ");
+        double totalVendas = Convert.ToDouble(Console.ReadLine());
+
+        double percentualADDsalario = totalVendas / percentual;
+
+        double salarioFinal = percentualADDsalario + salarioBase;
+
+        System.Console.WriteLine($"o salario do colaborador é: {salarioFinal} R$");
+        Console.ReadLine();
+    }
+}
