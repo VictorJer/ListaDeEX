f1e1b9504b4f710bdabd45905fefcd8d6611acb3
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-12 14:08:42 -0300

    ADD ex 01

--- ListaDeEX.ConsoleApp/Program.cs
+++ ListaDeEX.ConsoleApp/Program.cs
@@ -4,18 +4,18 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Digite o comprimento da caixa: ");
+        Console.Write("Digite o comprimento da caixa: ");
         decimal comprimento = Convert.ToDecimal(Console.ReadLine());
 
-        System.Console.WriteLine("Digite a largura da caixa: ");
+        System.Console.Write("Digite a largura da caixa: ");
         decimal largura = Convert.ToDecimal(Console.ReadLine());
 
-        System.Console.WriteLine("Digite a altura: ");
+        System.Console.Write("Digite a altura: ");
         decimal altura = Convert.ToDecimal(Console.ReadLine());
 
         decimal volume = comprimento * largura * altura;
 
-        System.Console.WriteLine("o volume da caixa é: " + volume);
+        System.Console.WriteLine("o volume da caixa é: " + volume + "cm³");
         Console.ReadLine();
     }
 }
