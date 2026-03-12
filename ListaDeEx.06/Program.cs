21e335aa6516999a19d434ddcdca1a0865614547
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-12 16:07:26 -0300

    ADD ex 06

---
+++ ListaDeEx.06/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        System.Console.Write("quantas notas o aluno tem: ");
+        int quantNotas = Convert.ToInt32(Console.ReadLine());
+
+        double notaSomada = 0;
+
+        for (int j = 1; j <= quantNotas; j++)
+        {
+            Console.Clear();
+             
+            System.Console.Write($"nota {j} do aluno: ");
+            notaSomada += Convert.ToDouble(Console.ReadLine());
+        }
+
+        Console.Clear();
+        
+        double media = notaSomada / quantNotas;
+
+        System.Console.WriteLine($"a media do aluno é {media.ToString("F2")}");
+
+
+        Console.ReadLine();
+    }
+}
+
+
+
+
+
+
+
+
+
+
