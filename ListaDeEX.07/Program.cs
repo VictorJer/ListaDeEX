27077edbde82aa0808622bd746bde5ec1075bb55
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-12 16:44:17 -0300

    ADD ex 07

---
+++ ListaDeEX.07/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double[] nota = new double[2];
+        double[] peso = new double[2];
+
+
+        for (int j = 0; j < 2; j++)
+        {
+            System.Console.WriteLine($"qual a nota {j + 1} do aluno: ");
+            nota[j] = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.WriteLine("qual o peso dela: ");
+            peso[j] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        double media = (nota[0] * peso[0] + (nota[1] * peso[1])) / (peso[0] + peso[1]);
+
+        System.Console.WriteLine($"a media ponderada é: {media}");
+
+        Console.ReadLine();
+    }
+}
