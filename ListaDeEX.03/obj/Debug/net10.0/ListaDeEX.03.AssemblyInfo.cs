0d2e096237bf0960ba6e3a42f3d5a6e995e3a9a7
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-12 14:41:59 -0300

    ADD gitignore

--- ListaDeEX.03/obj/Debug/net10.0/ListaDeEX.03.AssemblyInfo.cs
+++ ListaDeEX.03/obj/Debug/net10.0/ListaDeEX.03.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ListaDeEX.02")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ListaDeEX.03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b8b25dc8781b4e77d4064bb5cd758978218b30")]
-[assembly: System.Reflection.AssemblyProductAttribute("ListaDeEX.02")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ListaDeEX.02")]
+[assembly: System.Reflection.AssemblyProductAttribute("ListaDeEX.03")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ListaDeEX.03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
