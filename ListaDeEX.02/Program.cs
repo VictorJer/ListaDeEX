74b8b25dc8781b4e77d4064bb5cd758978218b30
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-12 14:33:20 -0300

    ADD ex 02

---
+++ ListaDeEX.02/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite o raio do cilindro: ");
+        double raio = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a altura do cilindro: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        double areaBase = 3.14 * raio * raio;
+
+        double volume = areaBase * altura;
+
+        System.Console.WriteLine($"o volume do ciclindro é {volume.ToString("F2")}");
+        Console.ReadLine();
+    }
+}
