a39c79ac310bfb861d98fd99723a7f31c05ad57e
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-13 14:48:02 -0300

    ADD ex 12

---
+++ ListaDeEX.12/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        System.Console.Write("qual o seu salario: ");
+        double salario = Convert.ToDouble(Console.ReadLine());
+
+        double salario15 = salario + (salario * 0.15);
+        double salario8 = salario15 - (salario15 * 0.08);
+
+        System.Console.WriteLine($"salario base {salario}R$ com o aumento {salario15}R$ com imposto {salario8}R$");
+    }
+}
