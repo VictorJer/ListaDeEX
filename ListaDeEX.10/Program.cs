d76bf7ec7d2666135a7c4ff5cb35708fa0545a6e
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-13 14:36:03 -0300

    ADD ex 10

---
+++ ListaDeEX.10/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        System.Console.Write("quantidade de pães vendidos: ");
+        int paom = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.Write("quantidade de broas vendidas: ");
+        int broa = Convert.ToInt32(Console.ReadLine());
+
+        double total = (paom * 0.12) + (broa * 1.50);
+
+        double poupa = total * 0.1;
+
+        System.Console.WriteLine($"quantidete bruta {total}R$ você deve guardar na poupança {poupa}R$");
+    }
+}
