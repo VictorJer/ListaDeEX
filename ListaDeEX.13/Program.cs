49c7908396e400cc79dfbea7a825ae91ff615b59
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-13 14:57:05 -0300

    ADD ex 13

---
+++ ListaDeEX.13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        System.Console.Write("Digite o valor A: ");
+        double A = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.Write("DIgite o valor B: ");
+        double B = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.Write("DIgite o valor C: ");
+        double C = Convert.ToDouble(Console.ReadLine());
+
+        double soma = A + B;
+
+        if (soma < C)
+        {
+            System.Console.WriteLine("A soma de A + B é menor que C");
+            Console.ReadLine();
+        }
+        else
+        {
+            System.Console.WriteLine("A soma de A + B é maior que C");
+        }
+    }
+}
