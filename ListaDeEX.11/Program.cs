30ad297a1baca74587a7d53def34006f5ec7ab96
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-13 14:41:58 -0300

    ADD ex 11

---
+++ ListaDeEX.11/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        System.Console.Write("qual o seu nome: ");
+        string? nome = Console.ReadLine();
+
+        System.Console.Write($"quantos anos você tem {nome}: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        int dias = idade * 365;
+
+        System.Console.WriteLine($"{nome} você ja viveu {dias} dias!");
+        Console.ReadLine();
+    }
+}
