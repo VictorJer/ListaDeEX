f6c525c96a8fe05addccc2636cff1359e921d3ca
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-18 16:07:35 -0300

    ADD ex 20

---
+++ ListaDeEX.20/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite um número de 1 a 10: ");
+        int N = int.Parse(Console.ReadLine());
+
+        if (N < 1 || N > 10)
+        {
+            Console.WriteLine("Número inválido!");
+            return;
+        }
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine($"{i} x {N} = {i * N}");
+        }
+    }
+}
