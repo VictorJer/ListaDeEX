25886e127e20764574902e73aab092751bbb0689
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-18 15:01:22 -0300

    ADD ex 18

---
+++ ListaDeEX.18/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 100; i <= 200; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
