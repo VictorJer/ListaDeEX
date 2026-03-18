283e7867dfe9cd608c901419c5e5a003ec8c4d26
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-18 14:53:13 -0300

    ADD ex 17

---
+++ ListaDeEX.17/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite o numero A: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o numero B: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int C = 0;
+
+        if (a == b)
+        {
+            C = a + b;
+        }
+        else
+        {
+            C = a * b;
+        }
+
+        Console.WriteLine($"O valor de C é: {C}");
+    }
+}
