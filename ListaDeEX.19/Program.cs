bd58e70432956795c486f5792917ed4e82bcd523
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-18 15:05:53 -0300

    ADD ex 19

---
+++ ListaDeEX.19/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int soma = 0;
+
+        for (int i = 1; i <= 500; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma += i;
+            }
+        }
+
+        Console.WriteLine($"A soma dos números ímpares de 1 a 500 é: {soma}");
+    }
+}
